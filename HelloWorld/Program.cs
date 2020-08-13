8a70b9a50254afc8768da17d8c1d4a2848c9d425
songhongsuk <eshssong@naver.com>
2020-08-13 18:08:46 +0900

    "4. merge : hotfix -> master"

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,7 +9,8 @@ namespace HelloWorld
             Console.WriteLine("1. master : first commit");
             Console.WriteLine("2. new branch : hotfix");
             Console.WriteLine("3. hotfix : second commit");
-
+            Console.WriteLine("4. merge : hotfix -> master");
+            
             Console.ReadKey();
         }
     }
