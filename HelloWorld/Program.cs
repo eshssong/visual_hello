6b6f03c0f2cbe8bcc01873d09a892c94d64b70cc
songhongsuk <eshssong@naver.com>
2020-08-13 18:23:57 +0900

    "1. master : first commit"

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,11 +7,6 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("1. master : first commit");
-            Console.WriteLine("2. new branch : hotfix");
-            Console.WriteLine("3. hotfix : first commit");
-            Console.WriteLine("4. merge : hotfix -> master");
-            Console.WriteLine("5. master : second Commit");
-            Console.WriteLine("6. master : third Commit");
 
             Console.ReadKey();
         }
