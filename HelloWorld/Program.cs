cc8db751d16fbeceff583ebff67225011f8e813a
songhongsuk <eshssong@naver.com>
2020-08-13 18:06:37 +0900

    "3. hotfix : second commit"

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,6 +7,10 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("1. master : first commit");
+            Console.WriteLine("2. new branch : hotfix");
+            Console.WriteLine("3. hotfix : second commit");
+
+            Console.ReadKey();
         }
     }
 }
